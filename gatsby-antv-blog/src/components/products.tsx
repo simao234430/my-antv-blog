90b9e14b055170203fa801658851bc85161f62c2
simao234430 <simao234430@gmail.com>
2022-09-15 22:39:55 +0800

    :bug: bu
    bug fix

--- gatsby-antv-blog/src/components/products.tsx
+++ gatsby-antv-blog/src/components/products.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import classNames from 'classnames';
 import { useTranslation } from 'react-i18next';
-import Product from './Product';
+import Product from './product';
 import { CATEGORIES, getNewProducts, ProductType } from './getProducts';
 import { useChinaMirrorHost } from '../hooks';
 import * as styles from './product.module.less';
