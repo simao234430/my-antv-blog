514b3ecc0b533fafd847a8e8ba8d669d1f25321b
simao234430 <simao234430@gmail.com>
2022-09-15 21:30:51 +0800

    :sparkles: add announcement

--- gatsby-antv-blog/src/components/layout.tsx
+++ gatsby-antv-blog/src/components/layout.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import { useStaticQuery, graphql } from 'gatsby'
+import { useStaticQuery, graphql, withPrefix } from 'gatsby'
 import { getCurrentLangKey } from 'ptz-i18n'
 import i18n from 'i18next'
 import { initReactI18next, useTranslation } from 'react-i18next'
@@ -7,6 +7,8 @@ import Header from './header'
 import Footer, { OLD_SITE_DOMAIN } from './Footer';
 import { FooterProps } from 'rc-footer';
 import locale from '../../locale.json'
+import * as styles from './layout.module.less';
+import TopBanner from './TopBanner';
 
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
@@ -31,6 +33,10 @@ const Layout: React.FC<any> = ({ children, location, footerProps}) => {
             }
           githubUrl
           title
+          announcement {
+            zh
+            en
+          }
           languages {
             langs
             defaultLangKey
@@ -44,18 +50,31 @@ const Layout: React.FC<any> = ({ children, location, footerProps}) => {
       siteMetadata: {
         navs,
         githubUrl,
+        announcement,
         title,
         languages: { langs, defaultLangKey },
         docs,
       },
     },
   } = data
-
+  const lngs = ['zh', 'en'];
+  const pathPrefix = withPrefix('/').replace(/\/$/, '');
+  const path = location.pathname.replace(pathPrefix, '');
   const currentLangKey = getCurrentLangKey(
     langs,
     defaultLangKey,
     location!.pathname
   )
+
+  const isHomePage =
+    path === '/' ||
+    path === `/${currentLangKey}` ||
+    path === `/${currentLangKey}/`;
+
+    const isExamplePage =
+    location.pathname.includes('/examples/') &&
+    !location.pathname.endsWith('/gallery');
+
 
   useEffect(() => {
     if (i18n.language !== currentLangKey) {
@@ -63,16 +82,19 @@ const Layout: React.FC<any> = ({ children, location, footerProps}) => {
     }
   }, [currentLangKey])
   return (
-    <main>
+    <>
       <Header   navs={navs} location={location} currentLangKey={currentLangKey} docs={[]} />
-      {children}
+ 
+        <TopBanner announcement={announcement} />
+ 
+      <main className={styles.main}>{children}</main>
       <Footer
           githubUrl={githubUrl}
           rootDomain="https://antv.vision"
           location={location}
           {...footerProps}
         />
-    </main>
+    </>
   )
 }
 export default Layout
